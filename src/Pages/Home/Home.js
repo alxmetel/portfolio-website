c0dbeb3e7e278d5ad81d7fad6cd6eef121de459e
alxmetel <alxmetel@gmail.com>
2020-08-03 03:58:31 -0400

    Connected Redux and Firebase

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.scss';
-import headerVideo from '../../Assets/header-video-test.mp4';
+import headerVideo from '../../assets/header-video-test.mp4';
 
 const Home = () => {
+
+  useEffect(() => {
+    // projectsRef.once('value', snapshot => {
+    //   console.log(snapshot);
+    // })
+
+    fetchProjectsData();
+  })
+
+  const fetchProjectsData = () => {
+    const url = 'https://portfolio-website-18313.firebaseio.com/projects.json';
+
+    fetch(url).then(response => response.json()).then(data => {
+      console.log(data)
+    })
+  }
+
+
   return (
     <div className="page-content page-home">
       <section className="header-video-section">
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+// import * as firebase from 'firebase';
+
+// const config = {
+//   apiKey: "AIzaSyCNd_X3z20853ZkHMqfkHHaBQEIPPY5WKk",
+//   authDomain: "portfolio-website-18313.firebaseapp.com",
+//   databaseURL: "https://portfolio-website-18313.firebaseio.com",
+//   projectId: "portfolio-website-18313",
+//   storageBucket: "portfolio-website-18313.appspot.com",
+//   messagingSenderId: "1085545213895"
+// }
+// firebase.initializeApp(config);
+// const databaseRef = firebase.database().ref();
+
+// export const projectsRef = databaseRef.child("projects")
