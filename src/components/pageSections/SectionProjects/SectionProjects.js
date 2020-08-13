9ca2b4151a03a77e0baf580f667884f9fbd20500
alxmetel <alxmetel@gmail.com>
2020-08-13 04:39:30 -0400

    Connected DB to the slider

--- src/components/pageSections/SectionProjects/SectionProjects.js
+++ src/components/pageSections/SectionProjects/SectionProjects.js
@@ -1,102 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './SectionProjects.scss';
 import SlickSlider from '../../shared/SlickSlider/SlickSlider';
-import data from '../../../siteData.json';
+import siteData from '../../../siteData.json';
 import SectionTitle from '../../shared/SectionTitle/SectionTitle';
 import ProjectCard from '../../shared/ProjectCard/ProjectCard';
 import LinkButton from '../../shared/LinkButton/LinkButton';
+import { sortArrayByValue } from '../../../utilities/utilityFunctions';
 
-import myPhoto from '../../../assets/my-photo.jpeg'; // Remove!!!
+const SectionProjects = props => {
+  const { data } = props;
 
-const SectionProjects = () => {
+  const [dataIsReady, setDataIsReady] = useState(false);
 
   useEffect(() => {
-    fetchProjectsData();
-  }, []);
-
-  const fetchProjectsData = () => {
-    const url = 'https://portfolio-website-18313.firebaseio.com/projects.json';
-
-    fetch(url).then(response => response.json()).then(data => {
-      console.log(data)
-    })
-  }
-
-  const projects = [
-    {
-      id: 0,
-      title: "Visnyk NAPU",
-      description: "fghgfhgfhgfdfsdfdsfsdfd sdfjkldjf lkdsj flksd flksdfdsfkljdkldslk jfkdsf hkj fjksdh fkdjsfjkd jkfd hfjkfjkfjksdf  jdksfjks fjsdhfjkshfjksk fkfjk fk kjhfkjsdfkjsdhfkjjjkfjkds fkj  fkjshjksdh fjkdsfsf",
-      tech_stack: [0, 1],
-      image: myPhoto,
-      order: 1
-    },
-    {
-      id: 1,
-      title: "Jur Liga",
-      description: "fghgfhgfhgfdfsdfdsfsdfd sdfjkldjf lkdsj flksd flksdfdsfkljdkldslk jfkdsf hkj fjksdh fkdjsfjkd jkfd hfjkfjkfjksdf  jdksfjks fjsdhfjkshfjksk fkfjk fk kjhfkjsdfkjsdhfkjjjkfjkds fkj  fkjshjksdh fjkdsfsf",
-      tech_stack: [1],
-      image: myPhoto,
-      order: 0
-    },
-    {
-      id: 2,
-      title: "Buch Liga",
-      description: "fghgfhgfhgfdfsdfdsfsdfd sdfjkldjf lkdsj flksd flksdfdsfkljdkldslk jfkdsf hkj fjksdh fkdjsfjkd jkfd hfjkfjkfjksdf  jdksfjks fjsdhfjkshfjksk fkfjk fk kjhfkjsdfkjsdhfkjjjkfjkds fkj  fkjshjksdh fjkdsfsf",
-      tech_stack: [1],
-      image: myPhoto,
-      order: 0
-    },
-    {
-      id: 3,
-      title: "Biz Liga",
-      description: "fghgfhgfhgfdfsdfdsfsdfd sdfjkldjf lkdsj flksd flksdfdsfkljdkldslk jfkdsf hkj fjksdh fkdjsfjkd jkfd hfjkfjkfjksdf  jdksfjks fjsdhfjkshfjksk fkfjk fk kjhfkjsdfkjsdhfkjjjkfjkds fkj  fkjshjksdh fjkdsfsf",
-      tech_stack: [1],
-      image: myPhoto,
-      order: 0
-    },
-    {
-      id: 4,
-      title: "Portfolio",
-      description: "fghgfhgfhgfdfsdfdsfsdfd sdfjkldjf lkdsj flksd flksdfdsfkljdkldslk jfkdsf hkj fjksdh fkdjsfjkd jkfd hfjkfjkfjksdf  jdksfjks fjsdhfjkshfjksk fkfjk fk kjhfkjsdfkjsdhfkjjjkfjkds fkj  fkjshjksdh fjkdsfsf",
-      tech_stack: [1],
-      image: myPhoto,
-      order: 0
+    if (data.length != 0) {
+      setDataIsReady(true);
     }
-  ]
+  }, [data]);
 
-  const sliderSettings = {
-    centerMode: true,
-    infinite: true,
-    slidesToShow: 3,
-    speed: 500,
-    dots: true,
-    responsive: [
-      {
-        breakpoint: 1025,
-        settings: {
-          slidesToShow: 1,
-          centerPadding: "20%",
-          arrows: false
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          centerPadding: "10%",
-          arrows: false
-        }
-      }
-    ]
-  }
-
-  function renderProjectCards(projectsData) {
-    return projectsData.map((elem, i) => {
+  function renderProjectCards() {
+    return sortArrayByValue(data, 'desc').map(elem => {
       return (
         <ProjectCard
-          key={i}
-          image={elem.image}
+          key={elem.id}
+          image={elem.image_small}
           title={elem.title}
           description={elem.description}
         />
@@ -104,20 +31,56 @@ const SectionProjects = () => {
     })
   }
 
+  function buildSlider() {
+    const sliderSettings = {
+      centerMode: true,
+      infinite: true,
+      slidesToShow: 3,
+      speed: 500,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 1,
+            centerPadding: "20%",
+            arrows: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            centerPadding: "10%",
+            arrows: false
+          }
+        }
+      ]
+    }
+
+    if (dataIsReady) {
+      return (
+        <SlickSlider
+          settings={sliderSettings}
+          slides={renderProjectCards()}
+        />
+      )
+    } else {
+      return null;
+    }
+  }  
+
   return (
     <section className="page-section section-projects">
       <div className="content-container">
-        <SectionTitle content={data.siteData.home.projects.title} />
+        <SectionTitle content={siteData.home.projects.title} />
         <div className="slider-container">
-          <SlickSlider
-            settings={sliderSettings}
-            slides={renderProjectCards(projects)}
-          />
+          {buildSlider()}
         </div>
         <div className="button-container">
           <LinkButton
             path="/projects"
-            title={data.siteData.home.projects.button_all_projects}
+            title={siteData.home.projects.button_all_projects}
           />
         </div>
       </div>
--- src/components/pages/PageIndexProjects/PageIndexProjects.js
+++ src/components/pages/PageIndexProjects/PageIndexProjects.js
@@ -2,17 +2,6 @@ import React, { useEffect } from 'react';
 
 const PageIndexProjects = () => {
   
-  useEffect(() => {
-    fetchProjectsData();
-  }, []);
-
-  const fetchProjectsData = () => {
-    const url = 'https://portfolio-website-18313.firebaseio.com/projects.json';
-
-    fetch(url).then(response => response.json()).then(data => {
-      console.log(data)
-    })
-  }
 
   return (
     <div className="page-index-projects">
