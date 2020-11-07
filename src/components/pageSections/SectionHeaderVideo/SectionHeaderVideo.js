5918e53d9d2fa79ec5896b3d074ca9c91d10e2f3
alxmetel <alxmetel@gmail.com>
2020-11-07 02:14:36 -0500

    Set poster for the header video

--- src/components/pageSections/SectionHeaderVideo/SectionHeaderVideo.js
+++ src/components/pageSections/SectionHeaderVideo/SectionHeaderVideo.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './SectionHeaderVideo.scss';
 import headerVideo from '../../../assets/header-video.mp4';
+import headerVideoPoster from '../../../assets/header-video-poster.jpg';
 
 const SectionHeaderVideo = () => {
   return (
     <section id="header-video" className="header-video-section">
-      <video autoPlay playsInline muted loop>
+      <video
+        poster={headerVideoPoster}
+        autoPlay
+        playsInline
+        muted
+        loop
+      >
         <source src={headerVideo} type="video/mp4"></source>
       </video>
       <div className="header-video-overlay"></div>
